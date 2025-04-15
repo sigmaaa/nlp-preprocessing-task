5b15003bb76e2d7b3bf0c51cc5314ddb776c3269
Denys Almazov <sigma@f0xz.org.ua>
2025-04-15 17:41:45 +0300

    init commit

---
+++ app.py
@@ -0,0 +1,14 @@
+def term_extraction(text):
+    #1) лексичний аналіз (на цьому етапі відсіюються всі символи, які не є буквами,
+    #наприклад: розділові знаки і html-теги);
+    #2) видалення стоп-слів;
+    #3) лематизація і стемінг.
+    pass
+
+def cleanup_text(text):
+    #Lexical analysis
+    #removing stop words
+    pass
+
+def lemmatization_and_steming(text):
+    pass
